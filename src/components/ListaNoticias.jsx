61fad1e16e905b21ddc2978602e82c146d61b18d
Draemiz <draemizgx1@gmail.com>
2026-06-17 23:22:06 -0500

    creado lista de noticias y modificado el App.jsx

---
+++ src/components/ListaNoticias.jsx
@@ -0,0 +1,28 @@
+import { useEffect, useState } from "react";
+import TarjetaNoticia from "./TarjetaNoticia";
+
+function ListaNoticias() {
+  const [noticias, setNoticias] = useState([]);
+
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/posts")
+      .then((response) => response.json())
+      .then((data) => {
+        setNoticias(data.slice(0, 12));
+      })
+      .catch((error) => console.error(error));
+  }, []);
+
+  return (
+    <div className="grid-noticias">
+      {noticias.map((noticia) => (
+        <TarjetaNoticia
+          key={noticia.id}
+          info={noticia}
+        />
+      ))}
+    </div>
+  );
+}
+
+export default ListaNoticias;
