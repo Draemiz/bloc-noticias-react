11742a11eb5809d076742fc79f15daeda603e46c
Draemiz <draemizgx1@gmail.com>
2026-06-17 23:06:58 -0500

    creado header de la appi bloc de noticias

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+function Header() {
+  return (
+    <header className="header">
+      <h1>Bloc de Noticias</h1>
+      <p>Noticias y artículos de actualidad</p>
+    </header>
+  );
+}
+
+export default Header;
